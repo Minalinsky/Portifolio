fcf799c8f7e7c4d6cc84756514b95af6a96db6af
Alyson <alyson1907@gmail.com>
2020-02-26 16:57:03 -0300

    +social media buttons

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Grid, Cell, ProgressBar } from 'react-mdl'
+import { Grid, Cell, ProgressBar, Button } from 'react-mdl'
 import ProfileCard from '../Components/ProfileCard'
 
 import '../Styles/LandingPage.css'
+
+import githubIcon from '../img/icons/github.svg'
+import gitlabIcon from '../img/icons/gitlab.svg'
+import linkedinIcon from '../img/icons/linkedin.svg'
 
 export default class LandingPage extends React.Component {
   constructor (props) {
@@ -12,7 +16,8 @@ export default class LandingPage extends React.Component {
         'REST APIs',
         'NodeJS',
         'MySQL'
-      ]
+      ],
+      socialMediaIcons: [gitlabIcon, githubIcon, linkedinIcon]
     }
   }
   render () {
@@ -21,11 +26,17 @@ export default class LandingPage extends React.Component {
         <Grid>
           <Cell col={12}>
             <ProfileCard />
-
             <div className='descriptionStyle description'>
               <h1> Back-End Developer </h1>
               <ProgressBar indeterminate style={{ width: '75%' }} />
               <h4> {this.state.hardSkills.join(' • ')}</h4>
+              <div className='socialMedia'>
+                {this.state.socialMediaIcons.map(icon => (
+                  <Button ripple accent className='socialButton'>
+                    <img src={icon} />
+                  </Button>
+                ))}
+              </div>
             </div>
           </Cell>
         </Grid>
