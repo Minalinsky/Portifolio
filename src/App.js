69ca568121967f1a3a568ad132e9ac80cd88a849
Alyson <alyson1907@gmail.com>
2020-02-26 17:26:34 -0300

    +comments @App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
+import { Layout, Header, Navigation, Content } from 'react-mdl'
 import { Link } from 'react-router-dom'
 import './Styles/App.css'
 import LandingPage from './Pages/LandingPage'
@@ -8,12 +8,15 @@ function App () {
   return (
     <div>
       <Layout>
+        {/* Header */}
         <Header transparent title='Alyson Maruyama' className='headerContainer'>
           <Navigation>
             <Link to='/'>Início</Link>
             <Link to='/about'>Sobre</Link>
           </Navigation>
         </Header>
+
+        {/* Body */}
         <Content>
           <LandingPage />
         </Content>
